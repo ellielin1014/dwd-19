bdb55456895e89d321f6bdf1d8e47e46fdef875f
Ellie Lin <ellielin1014@gmail.com>
2019-04-14 00:44:22 -0400

    add gitignore file

--- week-2/part-b/part-b.js
+++ week-2/part-b/part-b.js
@@ -11,22 +11,17 @@ app.set('views', __dirname);
 app.get('/', function(req, res) {
   if (req.query.form_input_name) {
     var min= 0;
-    var max= 3;  
+    var max= 3;
     var i = Math.floor(Math.random() * (max - min)) + min;
     res.send(req.query.form_input_name + insults[i]);
     return;
   }
-
     res.render('band', {
       template_variable_name: req.query.form_input_name
     });
   }
 )
 
-// app.get('/', function (req, res) {
-//   res.send("Our second route...");
-// }
-// )
 
 app.listen(8000, function () {
   console.log('Listen to port 8000')
