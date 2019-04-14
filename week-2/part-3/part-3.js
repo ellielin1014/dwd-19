f723fd2e4cab7279f522ac6ff7227119d52b2d29
Ellie Lin <ellielin1014@gmail.com>
2019-04-14 01:52:08 -0400

    modified testing

--- week-2/part-3/part-3.js
+++ week-2/part-3/part-3.js
@@ -9,12 +9,13 @@ app.set('view engine', 'html');
 app.set('views', __dirname);
 
 app.get('/', function(req, res) {
-  if (req.query.form_input_name) {
+  if (req.form_input_name) {
     var min= 0;
     var max= 3;
     var i = Math.floor(Math.random() * (max - min)) + min;
     res.send(req.query.form_input_name + insults[i]);
     return;
+    
   }
 
     res.render('band', {
@@ -22,11 +23,6 @@ app.get('/', function(req, res) {
     });
   }
 )
-
-// app.get('/', function (req, res) {
-//   res.send("Our second route...");
-// }
-// )
 
 app.listen(8000, function () {
   console.log('Listen to port 8000')
