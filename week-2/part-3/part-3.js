e242a18b68195f3ddaedbf6563b470a568f4334f
Ellie Lin <ellielin1014@gmail.com>
2019-04-26 19:01:59 -0400

    modification

--- week-2/part-3/part-3.js
+++ week-2/part-3/part-3.js
@@ -1,29 +1,33 @@
 var express = require('express');
 var mustacheExpress = require('mustache-express');
+var bodyParser = require("body-parser");
 
 var insults = ["? This band sucks!", "? Gross!", "? Ewww..."];
 var app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
+
 app.get('/', function(req, res) {
-  if (req.form_input_name) {
-    var min= 0;
-    var max= 3;
+  res.sendFile(__dirname + "/" + "band.html");
+})
+
+app.post('/', function(req, res) {
+  if (req.body.form_input_name) {
+    var min = 0;
+    var max = 3;
     var i = Math.floor(Math.random() * (max - min)) + min;
-    res.send(req.query.form_input_name + insults[i]);
+    res.send(req.body.form_input_name + insults[i]);
     return;
-    
-  }
 
-    res.render('band', {
-      template_variable_name: req.query.form_input_name
-    });
   }
-)
+})
 
-app.listen(8000, function () {
+app.listen(8000, function() {
   console.log('Listen to port 8000')
 })
